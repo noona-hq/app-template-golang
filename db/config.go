96fac118a1154744c3abf770648cd4701619b590
sverrirsig <svesig@gmail.com>
2024-03-11 09:09:20 +0000

    Database name change

--- db/config.go
+++ db/config.go
@@ -2,6 +2,6 @@ package db
 
 type Config struct {
 	Connection       string `default:"mongodb://localhost:31060"`
-	Name             string `default:"app_template"`
+	Name             string `default:"app-template"`
 	DirectConnection bool   `default:"true"`
 }
