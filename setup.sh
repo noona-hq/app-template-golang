c993d23593e16ac0c1fe09516cdf5275a52805c9
sverrirsig <svesig@gmail.com>
2024-03-11 10:16:18 +0000

    Use underscore in database name

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ read -p 'Enter the new project name: ' new_project_name
 # Template name
 old_name="app-template"
 
+# Convert new project name to use underscores instead of dashes for the database name
+new_db_name="${new_project_name//-/_}"  # Replace dashes with underscores for the database name
+
+# Update the db/config.go file with the new, underscored database name
+sed -i '' -e "s|$old_name|$new_db_name|g" db/config.go
+
 # Global find and replace the old name with the new name, excluding this script file
 # It's important to exclude any .git directory, this script, or other directories/files you wish to exclude
 find . -type f -not -path '*/\.*' -not -name 'setup.sh' -exec sed -i '' -e "s|$old_name|$new_project_name|g" {} +
