7931749521a6c13b784a8df6b406c4ea382e9a53
sverrirsig <svesig@gmail.com>
2024-05-21 15:21:12 +0000

    Use production values for template by default

--- services/noona/config.go
+++ services/noona/config.go
@@ -1,10 +1,13 @@
 package noona
 
 type Config struct {
-	BaseURL         string `default:"http://localhost:31140"`
-	AppStoreURL     string `default:"http://localhost:31130/week#settings-apps"`
-	ClientID        string `default:""`
-	ClientSecret    string `default:""`
-	AppBaseURL      string `default:"http://localhost:8080"`
+	BaseURL      string `default:"https://api.noona.is"`
+	AppStoreURL  string `default:"https://hq.noona.app/week#settings-apps"`
+	ClientID     string `default:""` // TODO: Fill in with your client ID
+	ClientSecret string `default:""` // TODO: Fill in with your client secret
+	// Our app will rely on webhooks to implement the Blacklist.
+	// We need to know where the webhook should be sent.
+	AppBaseURL string `default:"http://localhost:8080"`
+	// The Bearer token that all webhooks will be sent with
 	AppWebhookToken string `default:"very-secure-token-secret"`
 }
